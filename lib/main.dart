d29f6546554eee783c0a944ef496090afa1f1e00
Alex macbookAir <aleksandrmisnov@gmail.com>
2021-11-04 18:59:29 +0000

    added transaction class and a list to the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,14 +14,43 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Expense App'),
       ),
-      body: Center(
-        child: Text('Expense App starting setup!'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Card(
+            child: Container(
+              child: Text('Chart goes here'),
+              width: 100,
+            ),
+            color: Colors.blue,
+            elevation: 5,
+          ),
+          Card(
+            color: Colors.red,
+            child: Container(child: Text('List of expenses')),
+          ),
+        ],
       ),
     );
   }

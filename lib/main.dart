f021abaff510dd6f6c51ea7475ba46ed7a3d5a8c
Alex macbookAir <aleksandrmisnov@gmail.com>
2021-11-05 22:17:21 +0000

    start of the chart

--- lib/main.dart
+++ lib/main.dart
@@ -105,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             Container(
               child: Card(
-                child: Text('Chart goes here'),
+                child: Text('Chart will go here'),
                 color: Colors.blue,
                 elevation: 5,
               ),
